936cc452604ca5416817b4a45e44dede5a23d1a6
Muhammad Numan <muhammad.numan@ucd.ie>
2026-02-10 11:41:11 +0000

    updated make.jl file

--- docs/make.jl
+++ docs/make.jl
@@ -8,12 +8,6 @@ DocMeta.setdocmeta!(
     recursive = true,
 )
 
-const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
-const numbered_pages = [
-    file for file in readdir(joinpath(@__DIR__, "src")) if
-    file != "index.md" && splitext(file)[2] == ".md"
-]
-
 makedocs(;
     modules = [ElectricityNetworkReduction],
     authors = "Germán Morales-España <german.morales@tno.nl>, Juan Giraldo Chavarriaga <juan.giraldo@tno.nl>, Muhammad Numan <muhammad.numan@ucd.ie>, Ni Wang <ni.wang@tno.nl>",
@@ -22,7 +16,14 @@ makedocs(;
     format = Documenter.HTML(;
         canonical = "https://TulipaEnergy.github.io/ElectricityNetworkReduction.jl",
     ),
-    pages = ["index.md"; numbered_pages],
+    pages = [
+        "Home" => "index.md",
+        "Introduction" => "01-introduction.md",
+        "Mathematical Formulation" => "02-mathematical-formulation.md",
+        "Model Usage" => "03-model-usage.md",
+        "API Reference" => "04-api.md",
+        "Contributing Guidelines" => "05-contributing.md",
+    ],
 )
 
 deploydocs(; repo = "github.com/TulipaEnergy/ElectricityNetworkReduction.jl")
